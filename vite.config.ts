7919724b0ca92666bbc799f089a756f6adfb631a
Eydra624 <eydrachidimma@gmail.com>
2025-11-22 16:59:44 +0100

    Add CI/CD

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/user-web-app/",  
+  plugins: [react()],
+  base: "/user-web-app/",
 });
